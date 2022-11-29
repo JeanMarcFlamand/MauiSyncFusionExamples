d178f28161afd4feae59f1415a977dff2119379e
Jean-Marc Flamand <7jtf@videotron.ca>
2022-11-29 13:39:40 -0500

    Add project files.

---
+++ DataGrid/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DataGrid;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ DataGrid/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Syncfusion.Maui.DataGrid;
+
+namespace DataGrid;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+        SfDataGrid dataGrid = new SfDataGrid();
+        this.Content = dataGrid;
+    }
+
+	
+}
+
